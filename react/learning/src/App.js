f179a27e842b76ec4af382975c32c2c707576b8f
Bhavya Patel <93842768+BhavyaPatel305@users.noreply.github.com>
2023-08-14 09:48:16 +0530

    Sample commit2

--- react/learning/src/App.js
+++ react/learning/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 // When importing arrow functions, you always need to put them in {}
 import { HomeComponent } from './components/HomeComponent';
 import { UserComponent } from './propsdemo/UserComponent';
+import { StudentReg } from './components/StudentReg';
 
 function App() {
     var title = "Bhavya"
@@ -17,7 +18,8 @@ function App() {
         and the second title is the value of the key
         Here we are basically passing values to function in UserComponent file
     */}   
-      <UserComponent title = {title}/>
+      {/*<UserComponent title = {title}/>*/}
+      <StudentReg />    
     </div>
   );
 }
